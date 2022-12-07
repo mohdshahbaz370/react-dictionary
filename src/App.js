c11bd13cec66b4f5a4b0bc6aec8f3a69bdbcc6a1
mohdshahbaz <mohdshahbaz027@gmail.com>
2022-12-07 22:40:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Axios from "axios";
 import "./App.css";
 import { FaSearch } from "react-icons/fa";
 import { FcSpeaker } from "react-icons/fc";
+import Header from "./Header";
 
 function App() {
   // Setting up the initial states using react hook 'useState'
@@ -28,58 +29,37 @@ function App() {
   }
 
   return (
-    <div className="App">
-      <h1>Free Dictionary</h1>
-      <div className="searchBox">
-
-		//Taking user input
-        <input
-          type="text"
-          placeholder="Search..."
-          onChange={(e) => {
-            setSearchWord(e.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            getMeaning();
-          }}
-        >
-          <FaSearch size="20px" />
-        </button>
-      </div>
-      {data && (
-        <div className="showResults">
-          <h2>
-            {data.word}{" "}
-            <button
-              onClick={() => {
-                playAudio();
-              }}
-            >
-              <FcSpeaker size="26px" />
+    <>
+      <Header />
+      <div className="container">
+        <div className="d-flex flex-column align-items-center">
+          <div id="searchBox" class="my-4 w-50 mb-5 input-group">
+            <input className="form-control" type="text" placeholder="Search..." onChange={(e) => { setSearchWord(e.target.value); }} />
+            <button type="button" className="btn btn-outline-danger input-group-text" onClick={() => { getMeaning(); }}>
+              <FaSearch size="20px" />
             </button>
-          </h2>
-          <h4>Parts of speech:</h4>
-
-
-          <p>{data.meanings[0].partOfSpeech}</p>
-
-
-          <h4>Definition:</h4>
-
-
-          <p>{data.meanings[0].definitions[0].definition}</p>
-
-
-          <h4>Example:</h4>
-
-
-          <p>{data.meanings[0].definitions[0].example}</p>
-
+          </div>
         </div>
-      )}
-    </div>
+        <div className="displayBox">
+          {data && (
+            <>
+              <h2>
+                {data.word}{" "}
+                <button type="button" onClick={() => { playAudio(); }}>
+                  <FcSpeaker size="26px" />
+                </button>
+              </h2>
+              <h4>Parts of speech:</h4>
+              <p>{data.meanings[0].partOfSpeech}</p>
+              <h4>Definition:</h4>
+              <p>{data.meanings[0].definitions[0].definition}</p>
+              <h4>Example:</h4>
+              <p>{data.meanings[0].definitions[0].example}</p>
+            </>
+          )}
+        </div>
+      </div>
+    </>
   );
 }
 
