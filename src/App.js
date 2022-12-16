08e1a30992ec113e5a021649f8f6452f6893034d
mohdshahbaz <mohdshahbaz027@gmail.com>
2022-12-16 08:53:17 +0530

    set Header with color and other logic

--- src/App.js
+++ src/App.js
@@ -9,17 +9,18 @@ function App() {
   // Setting up the initial states using react hook 'useState'
 
   const [data, setData] = useState("");
-  const [searchWord, setSearchWord] = useState("car");
+  const [searchWord, setSearchWord] = useState("");
+  const [query, setQuery] = useState("car");
 
   useEffect(() => {
     getMeaning();
-  }, [searchWord])
+  }, [query])
 
   // Function to fetch information on button
   // click, and set the data accordingly
   function getMeaning() {
     Axios.get(
-      `https://api.dictionaryapi.dev/api/v2/entries/en_US/${searchWord}`
+      `https://api.dictionaryapi.dev/api/v2/entries/en_US/${query}`
     ).then((response) => {
       setData(response.data[0]);
     });
@@ -32,37 +33,46 @@ function App() {
     audio.play();
   }
 
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(searchWord);
+    //setSearch("");
+  }
   return (
-    <div className="container-fluid pb-5">
+    <>
       <Header />
-      <div className="d-flex flex-column align-items-center">
-        <img src={require("./books.jpg")} alt="books" width="450" />
-        <div id="searchBox" class="my-4 w-50 mb-5 input-group">
-          <input className="form-control" type="text" placeholder="Search..." onChange={(e) => { setSearchWord(e.target.value); }} />
-          <button type="button" className="btn btn-outline-danger input-group-text" onClick={() => { getMeaning(); }}>
-            <FaSearch size="20px" />
-          </button>
+      <div className="container-fluid pb-5">
+        <form onSubmit={getSearch} >
+          <div className="d-flex flex-column align-items-center">
+            <img src={require("./books.jpg")} className="my-3" alt="books" width="450" />
+            <div id="searchBox" class="my-4 w-50 mb-5 input-group">
+              <input className="form-control" type="text" placeholder="Search..." onChange={(e) => { setSearchWord(e.target.value); }} />
+              <button type="submit" className="btn btn-outline-danger input-group-text">
+                <FaSearch size="20px" />
+              </button>
+            </div>
+          </div>
+        </form>
+        <div className="displayBox">
+          {data && (
+            <>
+              <h2>
+                {data.word}{" "}
+                <button type="button" onClick={() => { playAudio(); }}>
+                  <FcSpeaker size="26px" />
+                </button>
+              </h2>
+              <h4>Parts of speech:</h4>
+              <p>{data.meanings[0].partOfSpeech}</p>
+              <h4>Definition:</h4>
+              <p>{data.meanings[0].definitions[0].definition}</p>
+              <h4>Example:</h4>
+              <p>{data.meanings[0].definitions[0].example}</p>
+            </>
+          )}
         </div>
       </div>
-      <div className="displayBox">
-        {data && (
-          <>
-            <h2>
-              {data.word}{" "}
-              <button type="button" onClick={() => { playAudio(); }}>
-                <FcSpeaker size="26px" />
-              </button>
-            </h2>
-            <h4>Parts of speech:</h4>
-            <p>{data.meanings[0].partOfSpeech}</p>
-            <h4>Definition:</h4>
-            <p>{data.meanings[0].definitions[0].definition}</p>
-            <h4>Example:</h4>
-            <p>{data.meanings[0].definitions[0].example}</p>
-          </>
-        )}
-      </div>
-    </div>
+    </>
   );
 }
 
